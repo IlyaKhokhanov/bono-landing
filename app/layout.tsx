b7658535c5e6cbb18bee074e9bdf1b1c3b0fe650
IlyaKhokhanov <hohanov.ilya@gmail.com>
2025-08-12 00:58:26 +0300

    change urls and lense effect

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  metadataBase: new URL('http://192.168.1.108:3000'),
+  metadataBase: new URL('https://wivo.netlify.app'),
   title: 'WIVO | Bank',
   description:
     'Introducing our innovative banking website that we can design for your unique needs. Offering both physical and virtual debit cards in addition to a user-friendly banking app. Create your personalized site with customized functionalities today.',
@@ -23,10 +23,10 @@ export const metadata: Metadata = {
     title: 'WIVO | Bank',
     description:
       'Introducing our innovative banking website that we can design for your unique needs. Offering both physical and virtual debit cards in addition to a user-friendly banking app. Create your personalized site with customized functionalities today.',
-    url: 'http://192.168.1.108:3000',
+    url: 'https://wivo.netlify.app',
     images: [
       {
-        url: './images/OG.jpg',
+        url: 'https://wivo.netlify.app/images/OG.jpg',
         width: 1200,
         height: 630,
         alt: 'WIVO Bank',
